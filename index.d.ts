93265f330fa686c9d8502a5cb7f6394f6a0e38d1
basil.belokon <basil.belokon@gmail.com>
2017-11-29 20:22:22 +0600

    append ConnectionPoolBind interface

--- index.d.ts
+++ index.d.ts
@@ -5,12 +5,21 @@ declare namespace wrap {
   interface QueryableBind {
     query(text: string, params?: any, callback?: (error: Error, results: ResultSet) => void): Query
   }
+
+  interface ConnectionPoolBind {
+    query(text: string, params?: any, callback?: (error: Error, results: ResultSet) => void): Query
+    acquire(callback: (error: Error, result: Connection) => void): void;
+    release(connection: Connection): void;
+    close(callback?: (error: Error) => void): void;
+  }
+
   interface IAnyDbBind {
     createConnection(url: string, callback?: (error: Error, connection: Connection) => void): QueryableBind;
     createConnection(opts: ConnectOpts, callback?: (error: Error, connection: Connection) => void): QueryableBind;
-    createPool(url: string, config: PoolConfig): QueryableBind;
-    createPool(opts: ConnectOpts, config: PoolConfig): QueryableBind;
+    createPool(url: string, config: PoolConfig): ConnectionPoolBind;
+    createPool(opts: ConnectOpts, config: PoolConfig): ConnectionPoolBind;
   }
+
   interface IAnyDb {
     createConnection(url: string, callback?: (error: Error, connection: Connection) => void): Connection;
     createConnection(opts: ConnectOpts, callback?: (error: Error, connection: Connection) => void): Connection;
